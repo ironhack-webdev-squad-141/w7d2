e72296d8f32a9b5dbbf6e98554c84b995367c196
mjarraya <mjarraya@student.42.fr>
2019-07-16 15:25:26 +0200

    list setup

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
 import "./App.css";
-import User from "./components/User";
-import CounterButton from "./components/CounterButton";
+import data from "./data";
+// import User from "./components/User";
+// import CounterButton from "./components/CounterButton";
 
-const App = () => {
-  return (
-    <div className="App">
-      <User firstName="John" lastName="Doe" />
-      <User firstName="Jane" lastName="Doe" />
-      <User firstName="Michael" />
-      <CounterButton />
-    </div>
-  );
-};
+class App extends React.Component {
+  state = {
+    movies: data
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        {/* <User firstName="John" lastName="Doe" />
+          <User firstName="Jane" lastName="Doe" />
+          <User firstName="Michael" />
+          <CounterButton /> */}
+      </div>
+    );
+  }
+}
 
 export default App;
